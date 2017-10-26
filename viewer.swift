1f2a391337b106bc7423919e397fbba81fe01959
Aether Lazuli <32772901+aeth1r@users.noreply.github.com>
2017-10-26 19:14:10 +0100

    Update viewer.swift

--- viewer.swift
+++ viewer.swift
@@ -3,5 +3,7 @@ class ProductViewController: UIViewController {
  @IBOutlet weak var productNameLabel: UILabel!
  override func viewDidLoad() {
  super.viewDidLoad()
+  
+  productNameLabel.text = "1937 Desk Phone"
  }
 }
